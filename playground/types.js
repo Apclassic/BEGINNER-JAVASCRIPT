177af4a39727838b0e5f59a105b8e29ac58a73e7
apclassic <brodeybriggs@hotmail.com>
2022-12-01 21:00:45 -0600

    Notes about string types.

---
+++ playground/types.js
@@ -0,0 +1,18 @@
+/* eslint-disable */
+const name = "brodey";
+const middle = "william";
+const last = `briggs`;
+
+const house = "she's so cool";
+const song = `Luke 
+join 
+
+the dark 
+side`;
+const hello = "hello my name is " + first + " Nice to meet you";
+
+// let hello2 = 'hello my name is '; //this is a terrible solution
+// hello2 = hello2 + first;
+// hello2 = hello2 + ' Nice to meet you';
+
+const intro = `hello my name is ${name}. Nice to meet you. I am ${1 +100} years old`;
