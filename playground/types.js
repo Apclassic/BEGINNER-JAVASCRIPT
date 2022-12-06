10541e928c35ffba1e1d68602e98f4f1551a54e1
apclassic <brodeybriggs@hotmail.com>
2022-12-05 22:59:44 -0600

    More numbers practice. *Yawn*

--- playground/types.js
+++ playground/types.js
@@ -11,8 +11,17 @@ the dark
 side`;
 const hello = "hello my name is " + first + " Nice to meet you";
 
-// let hello2 = 'hello my name is '; //this is a terrible solution
-// hello2 = hello2 + first;
-// hello2 = hello2 + ' Nice to meet you';
+let hello2 = 'hello my name is '; //this is a terrible solution
+hello2 = hello2 + first;
+hello2 = hello2 + ' Nice to meet you';
 
 const intro = `hello my name is ${name}. Nice to meet you. I am ${1 +100} years old`;
+
+// Numbers notes 
+const a = 10;
+const b = 20;
+const smarties = 20;
+const kids = 3;
+const eachKidGets = math.floor(smarties / kids);
+const dadGets = smarties % kids;
+console.log(`Each kids gets ${eachKidGets}`);
